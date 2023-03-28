ed99412b8857f68cabce413dbd191b654fd3986c
YeonHanByeol <hbv@kakao.com>
2023-03-28 15:56:27 +0900

    commit test

---
+++ fasoo-rest-api/src/main/java/com/fasoo/prac/FasooRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.fasoo.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasooRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasooRestApiApplication.class, args);
+	}
+
+}
